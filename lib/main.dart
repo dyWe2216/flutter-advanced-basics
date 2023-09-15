54b0208ea602c0bcde2f546631708dd981acbdb3
dyWe2216 <dnleodud@gmail.com>
2023-09-15 09:02:00 +0900

    feat: 퀴즈 시작 화면 코드 정리
    - startScreen 클래스 분리
    - 불필요한 코드 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,20 @@
+import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 31, 0, 83),
-            Color.fromARGB(255, 71, 2, 84)
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
         ),
-        child: Center(child: AnyName()),
       ),
     ),
-  ));
-}
-
-class AnyName extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          "assets/images/quiz-logo.png",
-          width: 300,
-        ),
-        const SizedBox(
-          height: 80,
-        ),
-        const Text(
-          'Learn Flutter the fun way!',
-          style: TextStyle(
-              fontSize: 28, color: Colors.white, fontWeight: FontWeight.w300),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Start Quiz',
-              style: TextStyle(color: Colors.white),
-            )),
-      ],
-    );
-  }
+  );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 28,
+              color: Color.fromARGB(255, 215, 137, 229),
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                foregroundColor: Color.fromARGB(255, 215, 137, 229)),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
